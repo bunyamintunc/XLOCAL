eef12fd906a6774ebb55943975a5390ad7b4b076
Bünyamin Tünç <bunyamintuncc@gmail.com>
2022-12-11 02:56:02 +0300

    Add unfollow action

--- app/src/main/java/com/tunc/xlocal/UserInfoActivity.java
+++ app/src/main/java/com/tunc/xlocal/UserInfoActivity.java
@@ -1,5 +1,6 @@
 package com.tunc.xlocal;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
@@ -13,7 +14,9 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
@@ -37,11 +40,12 @@ public class UserInfoActivity extends AppCompatActivity {
     private String userUuid;
     private FirebaseFirestore firebaseFirestore;
     private FirebaseAuth auth;
-    private String userName,profilPhotoUrl;
-    private TextView textCountOfJoin,textCountOfComment,textCountOfComplaint;
+    private String userName, profilPhotoUrl;
+    private TextView textCountOfJoin, textCountOfComment, textCountOfComplaint;
     private User user;
     private ImageView userProfile;
-    public UserInfoActivity(){
+
+    public UserInfoActivity() {
         firebaseFirestore = FirebaseFirestore.getInstance();
         auth = FirebaseAuth.getInstance();
         getCurrentUser();
@@ -66,7 +70,12 @@ public class UserInfoActivity extends AppCompatActivity {
 
         btnFollow = binding.btnFollow;
         btnFollow.setOnClickListener(view1 -> {
-            sendFollowRequest();
+            if (btnFollow.getText().equals("FOLLOW")) {
+                sendFollowRequest();
+            } else if (btnFollow.getText().equals("UNFOLLOW")) {
+                unfollow();
+            }
+
 
         });
 
@@ -74,53 +83,51 @@ public class UserInfoActivity extends AppCompatActivity {
         isAFollowers();
 
 
-
-
     }
 
-    public void sendFollowRequest(){
+    public void sendFollowRequest() {
 
-        HashMap<String,Object> followRequestData = new HashMap<>();
-        followRequestData.put("request_owner_uuid",auth.getCurrentUser().getUid());
-        followRequestData.put("user_name",userName);
-        followRequestData.put("profil_photo_url",profilPhotoUrl);
+        HashMap<String, Object> followRequestData = new HashMap<>();
+        followRequestData.put("request_owner_uuid", auth.getCurrentUser().getUid());
+        followRequestData.put("user_name", userName);
+        followRequestData.put("profil_photo_url", profilPhotoUrl);
         firebaseFirestore.collection("Users").document(userUuid).collection("FollowRequests").add(followRequestData).addOnSuccessListener(documentReference -> {
-             //hiddenFollowButton();
+            //hiddenFollowButton();
         });
     }
 
-    public void getCurrentUser(){
+    public void getCurrentUser() {
         firebaseFirestore.collection("Users").document(auth.getCurrentUser().getUid()).addSnapshotListener((value, error) -> {
-             userName = value.getData().get("userName").toString();
-             profilPhotoUrl = value.getData().get("profilePhotoDowloadUrl").toString();
-             textCountOfJoin.setText(value.getData().get("countOfJoin").toString());
-             textCountOfComment.setText(value.getData().get("countOfComment").toString());
+            userName = value.getData().get("userName").toString();
+            profilPhotoUrl = value.getData().get("profilePhotoDowloadUrl").toString();
+            textCountOfJoin.setText(value.getData().get("countOfJoin").toString());
+            textCountOfComment.setText(value.getData().get("countOfComment").toString());
 
         });
     }
 
-    public void hiddenFollowButton(){
+    public void hiddenFollowButton() {
         btnFollow.setClickable(false);
-        btnFollow.setBackgroundColor(ContextCompat.getColor(this,R.color.teal_200));
+        btnFollow.setBackgroundColor(ContextCompat.getColor(this, R.color.teal_200));
 
     }
 
-    public void unFollowButton(){
-        btnFollow.setBackgroundColor(ContextCompat.getColor(this,R.color.teal_700));
+    public void unFollowButton() {
+        btnFollow.setText("UNFOLLOW");
     }
 
-    public void deleteFollowButton(){
-         btnFollow.setVisibility(View.GONE);
+    public void deleteFollowButton() {
+        btnFollow.setVisibility(View.GONE);
     }
 
-     // kullanicinin bu kişiye istek atıp atmadığına bakiyoruz
-    public void isThereRequestFollow(){
+    // kullanicinin bu kişiye istek atıp atmadığına bakiyoruz
+    public void isThereRequestFollow() {
         firebaseFirestore.collection("Users").document(userUuid).collection("FollowRequests").addSnapshotListener((value, error) -> {
-            if (value.isEmpty()){
-                Toast.makeText(this,"I am working",Toast.LENGTH_LONG).show();
-            }else{
-                for ( DocumentSnapshot document : value.getDocuments()){
-                    if (document.get("request_owner_uuid").equals(auth.getCurrentUser().getUid())){
+            if (value.isEmpty()) {
+                Toast.makeText(this, "I am working", Toast.LENGTH_LONG).show();
+            } else {
+                for (DocumentSnapshot document : value.getDocuments()) {
+                    if (document.get("request_owner_uuid").equals(auth.getCurrentUser().getUid())) {
                         hiddenFollowButton();
                     }
                 }
@@ -128,22 +135,23 @@ public class UserInfoActivity extends AppCompatActivity {
         });
     }
 
-    public void isAFollowers(){
+    public void isAFollowers() {
         firebaseFirestore.collection("Users").document(auth.getCurrentUser().getUid()).collection("Followers").addSnapshotListener((value, error) -> {
-           if (value.isEmpty()){
+            if (value.isEmpty()) {
 
-           }else{
-               for(DocumentSnapshot document: value.getDocuments()){
-                     if(document.get("user_uuid").equals(userUuid)){
-                         unFollowButton();
-                     }
-               }
-           }
+            } else {
+                for (DocumentSnapshot document : value.getDocuments()) {
+                    if (document.get("user_uuid").equals(userUuid)) {
+                        unFollowButton();
+
+                    }
+                }
+            }
 
         });
     }
 
-    public void getUserInfo(){
+    public void getUserInfo() {
 
         firebaseFirestore.collection("Users").document(userUuid).addSnapshotListener((value, error) -> {
 
@@ -154,6 +162,43 @@ public class UserInfoActivity extends AppCompatActivity {
             binding.textViewJoin.setText(value.getData().get("countOfJoin").toString());
             binding.textViewLike.setText(value.getData().get("countOfLike").toString());
             binding.textFriendList.setText(value.getData().get("countOfFollowers").toString());
+        });
+    }
+
+   // eger unfollow edilirse kullanicinin takipci listesine gidip kullaniciyi bulup siliyor. kullanici silinirse yeniden mevcut kullanic
+    // giris yapan kullanicinin takipci listesinden de siliniyor.
+    public void unfollow(){
+        firebaseFirestore.collection("Users").document(auth.getCurrentUser().getUid()).collection("Followers").addSnapshotListener((value, error) -> {
+            if (value.isEmpty()){
+
+            }else{
+                for (DocumentSnapshot document: value.getDocuments()){
+                    if (document.get("user_uuid").equals(userUuid)){
+                        firebaseFirestore.collection("Users").document(auth.getCurrentUser().getUid()).collection("Followers").document(document.getId()).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                firebaseFirestore.collection("Users").document(userUuid).collection("Followers").addSnapshotListener((value1, error1) -> {
+                                    if (value1.isEmpty()){
+
+                                    }else{
+                                        for (DocumentSnapshot document : value1.getDocuments()){
+                                            if (document.get("user_uuid").equals(auth.getCurrentUser().getUid())){
+                                                 firebaseFirestore.collection("Users").document(userUuid).collection("Followers").document(document.getId()).delete().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                     @Override
+                                                     public void onSuccess(Void unused) {
+                                                         btnFollow.setText("FOLLOW");
+                                                     }
+                                                 });
+                                            }
+                                        }
+                                    }
+                                });
+
+                            }
+                        });
+                    }
+                }
+            }
         });
     }
 
